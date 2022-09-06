e32c835c1e100ade5dae083a34d9706af14f38a2
Mauricio De Lima Lopes Assenço <mauassenco@Mauricios-MacBook-Air.local>
2022-09-05 23:12:27 -0300

    FIrst component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
 
   return (
-    <h1>Hello React</h1>
+    <>
+      <h1>Hello React</h1>
+      <NotificationButton />
+    </>
   )
-  
+
 }
 
 export default App
